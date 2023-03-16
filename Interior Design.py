fc477584e1e95b8ccda1fede7a91e0e15dc59c54
sk28352 <112308194+sk28352@users.noreply.github.com>
2023-03-16 21:40:43 +0530

    Update Interior Design.py

--- Interior Design.py
+++ Interior Design.py
@@ -1,1 +1,12 @@
-
+t = int(input())           
+for i in range(t):
+    #Accept 4 integers as input
+    X1,Y1,X2,Y2 = map(int, input().split()) 
+    style_1 = X1+Y1
+    style_2 = X2+Y2
+    
+    if (style_1)>(style_2):
+        print(style_2)
+        
+    elif (style_1)<(style_2):
+        print(style_1)
